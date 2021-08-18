20ce18d134a70ae100ab96b147f3b4ef46de41eb
ingcarlosmontenegro <ingcarlosmontenegro@gmail.com>
2021-08-17 19:22:42 -0500

    Update backend.tf
    Se actualiza el Bucket a donde se guardan los estados.

--- IAC/backend.tf
+++ IAC/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "terraform-monitor-provide-states-files"
+    bucket  = "terraform-backend-montenegro"
     key     = "s3event.tfstate"
     region  = "us-east-1"
   }
